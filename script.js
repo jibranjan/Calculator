8b1d9b87df6f338995de2dc7d925194d90f048f5
jibranjan <jibranjan77@gmail.com>
2024-01-21 01:38:24 +0530

    functional

--- script.js
+++ script.js
@@ -16,11 +16,18 @@ let result = "";
 
 for (let i = 0; i < buttonsEl.length; i++) {
   buttonsEl[i].addEventListener("click", function () {
+    // if (buttonsEl[i].innerText == ".") {
+    //   if (displayEl.innerText.includes(".")) {
+    //   } else {
+    //   }
+    // }
     if (numericArray.includes(buttonsEl[i])) {
-      //   if (previousOperand !== "") {
-      //     displayEl.innerHTML = "";
-      //   }
-      displayEl.innerText = displayEl.innerHTML + "" + buttonsEl[i].innerHTML;
+      let currentDisplayValue = displayEl.innerText;
+      if (["+", "-", "*", "/"].includes(currentDisplayValue)) {
+        displayEl.innerText = buttonsEl[i].innerText;
+      } else {
+        displayEl.innerText = displayEl.innerHTML + "" + buttonsEl[i].innerHTML;
+      }
       currentOperand = currentOperand + "" + buttonsEl[i].innerHTML;
       if (currentOperand.length > 12) {
         displayEl.innerHTML = "Error";
@@ -29,6 +36,7 @@ for (let i = 0; i < buttonsEl.length; i++) {
     if (buttonsEl[i].innerHTML == "A/C") {
       displayEl.innerText = "";
       currentOperand = "";
+      currentOperator = "";
       previousOperand = "";
       result = "";
     }
@@ -43,13 +51,17 @@ for (let i = 0; i < buttonsEl.length; i++) {
     }
     if (operatorArray.includes(buttonsEl[i])) {
       displayEl.innerText = buttonsEl[i].innerHTML;
+      console.log(currentOperator);
+      if (currentOperator !== "") {
+        previousOperand = eval(
+          previousOperand + "" + currentOperator + "" + currentOperand
+        );
+      } else {
+        previousOperand = currentOperand;
+      }
+      console.log(previousOperand);
       currentOperator = buttonsEl[i].innerHTML;
-      previousOperand = currentOperand;
-      //   operand2 = previousOperand;
-      //   previousOperand = eval(
-      //     // sirf + k liye chalega, - mai galat answer dikhayega, * aur / mai error dikhayega
-      //     operand2 + "" + currentOperator + "" + currentOperand
-      //   );
+      //   previousOperand = currentOperand;
       currentOperand = "";
     }
     if (buttonsEl[i].innerHTML == "=") {
